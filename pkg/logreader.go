f66033302d5b35822a5b891df5b37a47ced97c59
NguyenDevWS <nguyen@iconai.app>
2024-06-03 18:14:21 -0700

    Adding log reading logic

---
+++ pkg/logreader.go
@@ -0,0 +1,61 @@
+package pkg
+
+import "os"
+
+// ReadLastNLines reads the last N lines from a file and returns them as a slice of strings.
+// If the file has less than N lines, it returns all lines.
+// N must be a positive integer.
+// File must exist and a text file.
+func ReadLastNLines(filename string, n int) ([]string, error) {
+	if n <= 0 {
+		return nil, fmt.Errorf("N must be a positive integer")
+	}
+	
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Get file size
+	stat, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+	size := stat.Size()
+
+	// Seek from the end
+	var lines []string 
+	buf := make([]byte, 1)
+	line := ""
+
+	// Start reading from the end of the file
+	for i:=size-1; i>0 && len(lines) < n; i-- {
+		_, err := file.Seek(i, os.SEEK_SET)
+		if err != nil {
+			return nil, err
+		}
+
+		_, err = file.Read(buf)
+		if err != nil {
+			return nil, err
+		}
+
+		// If we find a newline character, we have a line
+		if buf[0] == '\n' {
+			if line != "" {
+				lines = append([]string{line}, lines...)
+				line = ""
+			}
+		} else {
+			line = string(buf) + line
+		}
+	}
+
+	// If we have a line that is not empty, add it to the list
+	if line != "" && len(lines) < n {
+		lines = append([]string{line}, lines...)
+	}
+
+	return lines, nil
+}
